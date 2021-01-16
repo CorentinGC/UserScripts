baf4256d10a0f01f7464e7e2c2c0aaf5f0b1e5d5
CorentinGC <corentingc@gmail.com>
2021-01-16 20:40:36 +0100

    upd name

--- TwitchChestClicker/TwitchChestClicker.js
+++ TwitchChestClicker/TwitchChestClicker.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         TwitchAutoClick
+// @name         TwitchChestClicker
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  AutoClick bonus chests on twitch pages
