66db1610869ec3e31f33c6458efe922d6d5fa93c
CorentinGC <CorentinGC@users.noreply.github.com>
2021-01-18 21:44:32 +0100

    upd 0.8

--- CoinPusherSimulatorBOT/CoinPusherSimulatorBOT.js
+++ CoinPusherSimulatorBOT/CoinPusherSimulatorBOT.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CoinPusherBot
 // @namespace    https://corentingc.fr
-// @version      0.7
+// @version      0.8
 // @description  Play to CoinPusherSimulator
 // @author       CorentinGC
 // @match        https://www.twitch.tv/coinpushersimulator
@@ -11,7 +11,7 @@
 // @downloadURL  https://raw.githubusercontent.com/CorentinGC/UserScripts/main/CoinPusherSimulatorBOT/CoinPusherSimulatorBOT.js
 // ==/UserScript==
 
-const VERSION = '0.7'
+const VERSION = '0.8'
 const DEFAULT_TIMER = 10
 const KEY_BOT_TIMER = 'bot_timer'
 
